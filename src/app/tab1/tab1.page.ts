ed9c7b8126f94dfe02da1145500f42080ad1a2ef
sylvain.mn@mediatree.fr <sylvain.mn@mediatree.fr>
2022-12-20 17:58:08 +0100

    #9 deck programs actions in json

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ProgramsService } from '../services/programs.service';
+import DeckActions from '../../assets/json/deck_actions.json';
+import DeckPrograms from '../../assets/json/deck_programs.json';
 
 type Action = {
   name: string;
@@ -87,38 +89,8 @@ export class Tab1Page {
   }
 
   getData() {
-    var actions_marks: Action[] = [
-      {"name": "hacker à la volée","mark": "aucune","test": "Hacking+LOG vs Firewall+INT","limite": "Corruption","type": "Complexe","page": 666}
-    ];
-    var category_marks: Category = {
-      name: "manipulation de marks",
-      show: true,
-      actions: actions_marks
-    };
-    var actions_divers: Action[] = [
-      {"name": "brouiller les signaux","mark": "4","test": "G. Elec+LOG","limite": "Attaque","type": "Complexe","page": 239}
-    ];
-    var category_divers: Category = {
-      name: "divers",
-      show: true,
-      actions: actions_divers
-    };
-    var category_list: Category[] = [category_marks, category_divers];
-
-    var programs_list: Program[];
-    programs_list = [
-      {"active": false, "installed": false, "name": "navigateur", "page": 247, "description": "temps de recherche matriciel / 2"},
-      {"active": false, "installed": false, "name": "édition", "page": 247, "description": "TdD + 2 pour \"Editer un fichier\""},
-    ];
-
-    var hacking_programs_list: Program[];
-    hacking_programs_list = [
-      {"active": false, "installed": false, "name": "exploitation", "page": 247, "description": "Corruption + 2 pour \"hacker à la volée\""},
-      {"active": false, "installed": false, "name": "furtivité", "page": 247, "description": "Corruption + 1"},
-    ];
-
     var recovered_data: Data;
-    recovered_data = new Data(category_list, programs_list, hacking_programs_list)
+    recovered_data = new Data(DeckActions, DeckPrograms["common_programs"], DeckPrograms["hacking_programs"])
     return recovered_data
   }
 
