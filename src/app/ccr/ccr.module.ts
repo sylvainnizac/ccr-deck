6f8451c90c080ee6794263df2bb6121c796c16a8
Sylvain MN <7799272+sylvainnizac@users.noreply.github.com>
2022-12-24 09:29:10 +0100

    #17 basic ccr in progress

--- src/app/ccr/ccr.module.ts
+++ src/app/ccr/ccr.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CcrPage } from './ccr.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { DisplayProgramComponentModule } from '../display-programs/display-programs.module';
+import { DisplayActionsComponentModule } from '../display-actions/display-actions.module';
 import { CcrPageRoutingModule } from './ccr-routing.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
+    DisplayProgramComponentModule,
+    DisplayActionsComponentModule,
     FormsModule,
-    ExploreContainerComponentModule,
     CcrPageRoutingModule
   ],
   declarations: [CcrPage]
--- src/app/ccr/ccr.page.ts
+++ src/app/ccr/ccr.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { ProgramsService } from '../services/programs.service';
+import { DeckService } from '../services/deck.service';
+import CCRActions from '../../assets/json/ccr_actions.json';
+import CCRPrograms from '../../assets/json/ccr_programs.json';
+import Devices from '../../assets/json/devices.json';
+import { Appareil } from '../interfaces/deck';
+import { Data } from '../interfaces/datastruct';
+
 @Component({
   selector: 'app-ccr',
   templateUrl: 'ccr.page.html',
@@ -7,6 +15,36 @@ import { Component } from '@angular/core';
 })
 export class CcrPage {
 
-  constructor() {}
+  data: Data
+  device: Appareil = new Appareil(Devices.ccr[0])
+  max_program: number = 0
+  current_program: number = 0
+  attributes_list: string[] = ["attaque", "corruption", "firewall", "TdD"]
+
+  constructor(
+    private deckService: DeckService, 
+    private programService: ProgramsService
+    ) {
+    this.deckService.changeBaseDevice(new Appareil(Devices.ccr[1]))
+    this.deckService.displayed_device.subscribe(data => this.device = data)
+
+    this.data = this.getData()
+
+    this.programService.setMaxProgram(this.device.indice)
+    this.programService.max_program.subscribe(data => this.max_program = data)
+    this.programService.current_program.subscribe(data => this.current_program = data)
+  }
+
+  getData() {
+    return new Data(CCRActions, CCRPrograms["common_programs"], CCRPrograms["hacking_programs"])
+  }
+
+  decreaseCondition() {
+    this.deckService.updateCondition(-1)
+  }
+
+  increaseCondition() {
+    this.deckService.updateCondition(1)
+  }
 
 }
